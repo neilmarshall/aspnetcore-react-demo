6da08208370ee2bbc4055c5f14937a7a052f8963
Neil Marshall <MarshalN@moodys.com>
2021-10-11 15:36:39 +0100

    add DbUp for database generation

---
+++ backend/QandA.DataBase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace QandA.DataBase
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var connectionString = args.FirstOrDefault() ?? "Server=.; Database=QandA; Trusted_connection=true";
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
